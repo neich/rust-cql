d1b621c32fa99668f75e8505d0d5a0331bdd6456
alvaro7rlz <alvaroberruezo@gmail.com>
2016-06-02 19:10:46 +0100

    Removed ChannelPool

--- src/node.rs
+++ src/node.rs
@@ -9,6 +9,7 @@ use std::error::Error;
 use std::thread;
 use std::sync::mpsc::channel;
 use std::sync::Arc;
+use std::time::{Instant,Duration};
 
 use def::*;
 use def::OpcodeRequest::*;
@@ -20,29 +21,30 @@ use connection::CqlMsg;
 use connection_pool::ConnectionPool;
 
 pub struct Node {
-    channel_pool: Arc<ChannelPool>, //Set of channels
+    channel_cpool: Sender<CqlMsg>, 
     pub version: u8,
     address: SocketAddr
 }
 
 impl Node{
     
-    pub fn new(address: SocketAddr,channel_pool: Arc<ChannelPool>) -> Node {
+    pub fn new(address: SocketAddr,channel_cpool: Sender<CqlMsg>) -> Node {
+
         Node{
-            channel_pool: channel_pool,
+            channel_cpool: channel_cpool,
             version: CQL_MAX_SUPPORTED_VERSION,
             address: address
         }
     }
     
-    pub fn set_channel_pool(&mut self,channel_pool: Arc<ChannelPool>){
+    pub fn set_channel_cpool(&mut self,channel_cpool: Sender<CqlMsg>){
         /*
         match self.channel_pool {
             Some(c) =>  panic!("Channel pool can only be set once"),
             None    =>  self.channel_pool = Some(channel_pool),
         }
         */
-        self.channel_pool = channel_pool;
+        self.channel_cpool = channel_cpool;
     }
 
     /*
@@ -56,8 +58,10 @@ impl Node{
     //pub fn start(&mut self){
     //    self.run_event_loop();
     //}
-    pub fn get_latency(&self) ->usize{
-        1
+    pub fn get_latency(&self) -> Duration{
+        let start = Instant::now();
+        self.exec_dummy_query().await();
+        Instant::now() - start 
     }
 
     pub fn exec_query(& self, query_str: &str, con: Consistency) -> CassFuture {
@@ -78,7 +82,7 @@ impl Node{
         self.exec_query(query,Consistency::One)
     }
 
-    pub fn exec_prepared(&mut self, preps: &Vec<u8>, params: &Vec<CqlValue>, con: Consistency) -> CassFuture{
+    pub fn exec_prepared(&self, preps: &Vec<u8>, params: &Vec<CqlValue>, con: Consistency) -> CassFuture{
         let q = CqlRequest {
             version: self.version,
             flags: 0x00,
@@ -89,13 +93,14 @@ impl Node{
         self.send_message(q)
     }
     
-    pub fn exec_batch(&mut self, q_type: BatchType, q_vec: Vec<Query>, con: Consistency) -> CassFuture {
+    pub fn exec_batch(&self, q_type: BatchType, q_vec: Vec<Query>, con: Consistency) -> CassFuture {
         let q = CqlRequest {
             version: self.version,
             flags: 0x00,
             stream: 0x01,
             opcode: OpcodeBatch,
-            body: RequestBatch(q_vec, q_type, con, 0)};
+            body: RequestBatch(q_vec, q_type, con, 0)
+        };
 
         /* Code to debug batch statements. Write to file the serialization of the request
 
@@ -112,7 +117,7 @@ impl Node{
     }
 
 
-    pub fn prepared_statement(&mut self, query_str: &str) -> RCResult<CqlPreparedStat> {
+    pub fn prepared_statement(&self, query_str: &str) -> RCResult<CqlPreparedStat> {
         let q = CqlRequest {
             version: self.version,
             flags: 0x00,
@@ -135,17 +140,10 @@ impl Node{
 
     fn send_message(&self,request: CqlRequest) -> CassFuture{
         let (tx, future) = Future::<RCResult<CqlResponse>, ()>::pair();
-        match self.channel_pool.find_available_channel(){
-            Ok(channel) => {
-                channel.send(CqlMsg::Request{
+        self.channel_cpool.send(CqlMsg::Request{
                                 request: request,
                                 tx: tx,
                                 address: self.address});
-            },
-            Err(e) => {
-                tx.complete(Err(RCError::new("Sending error", IOError)));
-            },
-        }
         future
     }
 
@@ -177,49 +175,27 @@ impl Node{
             opcode: OpcodeStartup,
             body: RequestStartup(body),
         };
-        match self.channel_pool.find_available_channel(){
-            Ok(channel) => {
-                channel.send(CqlMsg::Connect{
+        self.channel_cpool.send(CqlMsg::Connect{
                                 request: msg_startup,
                                 tx: tx,
                                 address: self.address});
-            },
-            Err(e) => {
-                tx.complete(Err(RCError::new("Sending error", IOError)));
-            },
-        }
         future
     }
 
     pub fn get_sock_addr(&self) -> SocketAddr{
         self.address
     }
-}
-   
 
-// The idea is to have a set of event loop channels to send 
-// the CqlRequests. This will be changed when we decide how 
-// to manage our event loops (connections) but for now it is
-// only use one event loop and so one channel
-pub struct ChannelPool {
-    channels: Vec<Sender<CqlMsg>>
-}
-
-impl ChannelPool {
-    pub fn new() -> ChannelPool {
-        ChannelPool {
-            channels: Vec::new()
-        }
-    }
-    pub fn add_channel(&mut self, channel: Sender<CqlMsg>){
-        self.channels.push(channel);
-    }
-    // For now only use one channel (and one event loop)
-    fn find_available_channel(&self) -> Result<&Sender<CqlMsg>,&'static str>{
-        if self.channels.len() > 0 {
-            return Ok(self.channels.last().unwrap());
-        }
-        Err("There is no channel created")
+    fn exec_dummy_query(& self) -> CassFuture {
+        let query_str = "SELECT now() FROM system.local;";
+        let con = Consistency::One;
+        let q = CqlRequest {
+            version: self.version,
+            flags: 0x00,
+            stream: 0x01,
+            opcode: OpcodeQuery,
+            body: RequestQuery(String::from(query_str), con, 0)};
+        self.send_message(q)
     }
 }
 
