6981456b731677b938768dcaee705ee9c37b2fa4
alvaro7rlz <alvaroberruezo@gmail.com>
2016-06-08 19:09:53 +0100

    Removed unimplemented custom types from test

--- tests/test_cql/mod.rs
+++ tests/test_cql/mod.rs
@@ -35,11 +35,11 @@ pub fn to_hex_string(bytes: &Vec<u8>) -> String {
 
 #[test]
 fn test_queries() {
-    println!("Connecting ...!");
+    
     let ip = "172.17.0.2";
     let port = "9042";
     let ip_port = ip.to_string()+":"+port;
-    
+    println!("Connecting to {:?}",ip_port);
     let mut cluster = Cluster::new();
     
     let mut response = try_test!(cluster.connect_cluster(ip_port.parse().ok().expect("Couldn't parse address")),
@@ -58,18 +58,15 @@ fn test_queries() {
                 bigint_ bigint,
                 blob_ blob,
                 boolean_ boolean,
-                date_ date,
                 decimal_ decimal,
                 double_ double,
                 float_ float,
                 inet_ inet,
                 int_ int primary KEY,
-                smallint_ smallint,
                 text_ text,
                 time_ time,
                 timestamp_ timestamp,
                 timeuuid_ timeuuid,
-                tinyint_ tinyint,
                 uuid_ uuid,
                 varchar_ varchar,
                 varint_ varint
@@ -79,15 +76,15 @@ fn test_queries() {
     response = try_test!(cluster.exec_query(q, cql::Consistency::One).await().unwrap(), "Error creating table rust.test_types");
     assert_response!(response);
     println!("Result: {:?} \n", response);
-    /*
+    
     q = "INSERT INTO rust.test_types
-        (int_, ascii_, bigint_, blob_, boolean_, date_, decimal_, double_, float_, inet_, smallint_, text_, time_, timestamp_, timeuuid_, tinyint_, uuid_, varchar_, varint_)
-        VALUES(0, 'asdf', 0, 0x00A1, false, toDate(now()), 1.0, 1.2345, 1.234567890123, '127.0.0.1', 13, 'asdf', '13:30:54.234', toTimestamp(now()), now(), 12, now(), 'qwerty', 27);";
+        (int_, ascii_, bigint_, blob_, boolean_, decimal_, double_, float_, inet_, text_, time_, timestamp_, timeuuid_, uuid_, varchar_, varint_)
+        VALUES(0, 'asdf', 156234, 0x00A1, false, 1.6, 1.2345, 1.234567890123, '127.0.0.1', 'asdf', '13:30:54.234', toTimestamp(now()), now(), now(), 'qwerty', 27);";
     println!("cql::Query: {}", q);
     response = try_test!(cluster.exec_query(q, cql::Consistency::One).await().unwrap(), "Error inserting into table test");
     assert_response!(response);
     println!("Result: {:?} \n", response);
-    */
+    
     q = "select * from rust.test_types";
     println!("cql::Query: {}", q);
     response = try_test!(cluster.exec_query(q, cql::Consistency::One).await().unwrap(), "Error selecting from table test");
@@ -100,7 +97,7 @@ fn test_queries() {
     println!("Created prepared with id = {}", to_hex_string(&preps.id));
 
     println!("Execute prepared");
-    let params = &vec![cql::CqlInt(Some(7)),CqlVarchar(Some(Cow::Borrowed("Jon Snow"))), cql::CqlBigInt(Some(1234567890))];
+    let params = &vec![cql::CqlInt(Some(7)),CqlVarchar(Some(Cow::Borrowed(""))), cql::CqlBigInt(Some(1234567890))];
     response = try_test!(cluster.exec_prepared(&preps.id, params, cql::Consistency::One).await().unwrap(), "Error executing prepared statement");
     assert_response!(response);
     println!("Result: {:?} \n", response);
