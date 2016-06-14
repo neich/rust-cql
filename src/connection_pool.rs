733cd15e11fef22dcf06e1bcf3273f750b8bd358
alvaro7rlz <alvaroberruezo@gmail.com>
2016-06-14 16:19:24 +0100

    Slab has now capacity for 32768 connections

--- src/connection_pool.rs
+++ src/connection_pool.rs
@@ -26,13 +26,13 @@ impl ConnectionPool {
     pub fn new(event_handler: Sender<CqlEvent>) -> ConnectionPool {
         ConnectionPool {
             token_by_ip: BTreeMap::new(),
-            connections: Slab::new_starting_at(Token(1), 128),
+            connections: Slab::new_starting_at(Token(1), 32768),
             event_handler: event_handler
         }
     }
     
     fn get_connection_with_ip(&mut self,event_loop: &mut EventLoop<ConnectionPool>,address:&IpAddr) -> Result<&mut Connection,&'static str>{
-        println!("[ConnectionPool::get_connection_with_ip]");
+        //println!("[ConnectionPool::get_connection_with_ip]");
         if !self.exists_connection_by_ip(address){
             //let token = self.create_connection(event_loop,address).unwrap();
             //return self.find_connection_by_token(token)
@@ -44,7 +44,7 @@ impl ConnectionPool {
     }
     
     fn create_connection(&mut self,event_loop: &mut EventLoop<ConnectionPool>,address:&IpAddr) -> RCResult<Token>{
-        println!("[ConnectionPool::create_connection]");
+        //println!("[ConnectionPool::create_connection]");
         let mut conn = try_rc!(connect(SocketAddr::new(address.clone(),9042),
                                 None,
                                 event_loop,
@@ -54,14 +54,14 @@ impl ConnectionPool {
     }
 
     fn add_connection(&mut self, address:IpAddr,connection: Connection)-> RCResult<Token>{
-        println!("[ConnectionPool::add_connection]");
+        //println!("[ConnectionPool::add_connection]");
         let result = self.connections.insert(connection);
 
         match result{
             Ok(token) => {
                 {
                 let conn = self.find_connection_by_token(token).ok().expect("Couldn't unwrap the connection");
-                println!("Setting token {:?}",token);
+                //println!("Setting token {:?}",token);
                 conn.set_token(token);
                 }
                 self.token_by_ip.insert(address,token);
@@ -83,23 +83,29 @@ impl ConnectionPool {
     }
 
     fn find_connection_by_ip(&mut self,address:&IpAddr) -> Result<&mut Connection,&'static str>{
-        println!("[ConnectionPool::find_connection_by_ip]");
+        //println!("[ConnectionPool::find_connection_by_ip]");
         if !self.connections.is_empty() {
             // Needs to be improved
-            return Ok(self.connections.get_mut(self.token_by_ip
+            let conn = 
+            Ok(self.connections.get_mut(self.token_by_ip
                                                    .get(address).unwrap().clone()
                                               ).unwrap());
+            //println!("{:?}",conn );
+            return conn;
         }
         Err("There is no connection found")
     }
 
     fn find_connection_by_token(&mut self,token: Token) -> Result<&mut Connection,&'static str>{
-        println!("[ConnectionPool::find_connection_by_token]");
+        //println!("[ConnectionPool::find_connection_by_token]");
         if !self.connections.is_empty() {
-            return Ok(self.connections.get_mut(token).unwrap());
+            let conn = Ok(self.connections.get_mut(token).unwrap());
+            //println!("Connection with {:?} -> {:?}",token,conn );
+            return conn;
         }
         Err("There is no connection found")
     }
+
 }
 
 
@@ -113,7 +119,8 @@ impl mio::Handler for ConnectionPool {
     // Push pending messages to be send across the event 
     // loop. Only change state in case it is waiting
     fn notify(&mut self, event_loop: &mut EventLoop<ConnectionPool>, msg: CqlMsg) {
-        println!("[ConnectionPool::notify]");
+        //println!("[ConnectionPool::notify]");
+        //self.show_connections();
         let ip = &msg.get_ip();
         //let complete = msg.get_complete();
         match msg {
@@ -123,8 +130,10 @@ impl mio::Handler for ConnectionPool {
                 // Connect, then send_startup with the queue_message
                 match result {
                     Ok(conn) =>{
-                        conn.insert_request(msg);
-                        conn.reregister(event_loop,EventSet::writable());
+                        match conn.insert_request(msg){
+                            Ok(_) => conn.reregister(event_loop,EventSet::writable()),
+                            Err(err) => (),
+                        }
                     },
                     Err(err) =>{
                         //TO-DO 
@@ -153,28 +162,26 @@ impl mio::Handler for ConnectionPool {
 
     
     fn ready(&mut self, event_loop: &mut EventLoop<ConnectionPool>, token: Token, events: EventSet) {
-        println!("[Connection::ready]");      
-        println!("Assigned token is: {:?}",token);
-        println!("Events: {:?}",events);
+        //println!("[Connection::ready]");      
+        //println!("Assigned token is: {:?}",token);
+        //println!("Events: {:?}",events);
         let mut connection = self.find_connection_by_token(token).ok().expect("Couldn't get connection");           
         if events.is_readable() {
-            println!("    connection-EventSet::Readable");
+            //println!("    connection-EventSet::Readable");
             connection.read(event_loop);
-
             let pair = connection.read_cql_response();
             let response = pair.0;
             let is_event = pair.1;
             //println!("Response from event_loop: {:?}",response);
-            println!("Handling response..");
+            //println!("Handling response..");
             connection.handle_response(response,event_loop,is_event);
             connection.reset_response();
         }
 
         if events.is_writable() && connection.are_pendings_send(){
-            println!("    connection-EventSet::Writable");
+            //println!("    connection-EventSet::Writable");
             connection.write(event_loop);
         }
-
-        println!("[Connection::Ended ready]");
+        //println!("[Connection::Ended ready]");
     }
 }
